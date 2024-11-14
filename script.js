ac31dc09929d2f94a64e8bbffdd91b3fdf65c279
Thuotracy <tracyjacobs775@gmail.com>
2024-11-14 14:49:23 +0300

    made chnages

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let container = document.querySelector('.container');
 
-for(let i = 1; i <= 2500; i++){
+for(let i = 1; i <= 3003; i++){
     let box = document.createElement('div');
     box.classList.add('box');
     container.appendChild(box);
